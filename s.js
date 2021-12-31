5028e86efcfb9ac62b3e5c8d74685a34b53db5a3
hologramgoose <89285672+hologramgoose@users.noreply.github.com>
2022-01-01 02:00:12 +0900

    Add files via upload

---
+++ s.js
@@ -0,0 +1,18 @@
+
+var image = new Array();
+image[0] = "../deokdam\\1 (1).png";
+image[1] = "../deokdam\\2.png";
+image[2] = "../deokdam\\3.png";
+image[3] = "../deokdam\\4.png";
+image[4] = "../deokdam\\7.png";
+function show()
+{
+    var number = Math.round(Math.random()*4);
+    var showImage = document.getElementById("img");
+    showImage.src = image[number];
+}
+
+
+
+
+ 

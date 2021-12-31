06559dee09410afe4e7a0dc70d7e2e2ade48ac8e
hologramgoose <89285672+hologramgoose@users.noreply.github.com>
2022-01-01 02:04:15 +0900

    Update s.js

--- s.js
+++ s.js
@@ -1,10 +1,10 @@
 
 var image = new Array();
-image[0] = "../deokdam\\1 (1).png";
-image[1] = "../deokdam\\2.png";
-image[2] = "../deokdam\\3.png";
-image[3] = "../deokdam\\4.png";
-image[4] = "../deokdam\\7.png";
+image[0] = "../deokdam\1 (1).png";
+image[1] = "../deokdam\2.png";
+image[2] = "../deokdam\3.png";
+image[3] = "../deokdam\4.png";
+image[4] = "../deokdam\7.png";
 function show()
 {
     var number = Math.round(Math.random()*4);
